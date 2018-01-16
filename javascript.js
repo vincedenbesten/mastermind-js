82d0e417de7c41585d5d6e45a246272f3e81c4f3
Vince den Besten <99043881@mydavinci.nl>
2018-01-16 13:08:50 +0100

    aantal rijen zelf te kiezen

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
+var rows = 12;
 
-for(row =1; row <= 12 ; row++) {
-	for(column = 1; column <= 4; column++) {
-    var div = document.createElement("div");
-    div.style.backgroundColor="green";
-    div.id = "row_" + row + "column_" + column;
-    document.getElementById("secenddis").appendChild(div);
-  }
+function start() {
+	rows = document.getElementById("myNumber").value;
+	for(c =1; c <= rows ; c++) {
+		var div = document.createElement("div");
+		div.id = "nakijkrow_" + c;
+		div.className = "nakijkrows";
+		document.getElementById("results").appendChild(div);
+
+		for(b = 1; b <= 4; b++) {
+	    var div = document.createElement("div");
+			div.className = "result";
+	    div.id = "row_" + c + "bolletje_" + b;
+	    document.getElementById("nakijkrow_" + c).appendChild(div);
+	  }
+	}
+
+	for(row =1; row <= rows ; row++) {
+		for(column = 1; column <= 4; column++) {
+	    var div = document.createElement("div");
+			div.className = "bolekes";
+	    div.id = "row_" + row + "column_" + column;
+	    document.getElementById("secenddis").appendChild(div);
+	  }
+	}
 }
+
+
 
 /*var r = 0;
 var c = 0;
